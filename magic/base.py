a4d53f5fe7d965ea53038bc2d79687b22f72fdc2
songokunr1 <pawelm18@gmail.com>
2021-04-06 20:55:31 +0200

    deep dive into magic

---
+++ magic/base.py
@@ -0,0 +1,25 @@
+import collections
+import dis
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class Deck():
+    numbers = [number for number in range(2, 11)] + list("JQKA")
+    ranks = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._card = [Card(rank, suit) for suit in self.numbers for rank in self.ranks]
+
+    def __repr__(self):
+        return 'Card(%r, %r)' % (10, 15)
+
+    def __len__(self):
+        return len(self._card)
+
+    def __getitem__(self, item):
+        return self._card[item]
+
+cards = Deck()
+print(dis.dis(Deck))
+
